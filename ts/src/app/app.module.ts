902e352fd2015550da8baa0b0f89e5865621c3cd
gustavokozlowski <guskozlowskidev@gmail.com>
2024-05-09 00:40:50 -0300

    refactor(src/app): da estrutura inicial da aplicação

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AsideComponent } from './components/aside/aside.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AsideComponent
+    AsideComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
