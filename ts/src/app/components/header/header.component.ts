3eec94966b5832e4f0c6fc75c1f69746618b10a5
gustavokozlowski <guskozlowskidev@gmail.com>
2024-05-09 20:52:43 -0300

    refactor: refactor do component header

--- ts/src/app/components/header/header.component.ts
+++ ts/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/@types/user';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  user: User = {
+    image: '../../../assets/images/user-image.jpg',
+    email: 'alguem12@galoascience.com',
+  }
 
-  constructor() { }
+  img = {
+    dropdown: "../../../assets/images/dropdown.png",
+    globe: "../../../assets/images/globe.png"
+  }
+  constructor(
+
+  ) { }
 
   ngOnInit(): void {
   }
