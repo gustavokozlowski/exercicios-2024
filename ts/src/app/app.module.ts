c81cdcf0ddd7eda9d25147b21b16b5bc3437963b
gustavokozlowski <guskozlowskidev@gmail.com>
2024-05-10 00:59:25 -0300

    feat: adiciona o roteamento de rotas

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AsideComponent } from './components/aside/aside.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AsideComponent,
     LogoComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
