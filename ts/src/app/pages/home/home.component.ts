fa5bec03b8ba389c4f7b1385a878b2dcb987905f
gustavokozlowski <guskozlowskidev@gmail.com>
2024-05-10 15:44:16 -0300

    refactor: adiciona novos elementos

--- ts/src/app/pages/home/home.component.ts
+++ ts/src/app/pages/home/home.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  video = "../../../assets/images/video.jpg";
+  btn = {
+    download: "../../../assets/images/download.png",
+    doi: "../../../assets/images/doi.png",
+    fav: "../../../assets/images/favoritar.png"
+  }
+  show: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleShow(): void {
+    this.show = true
+  }
 }
