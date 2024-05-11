c469cd0a967cef95993248640b426b17ee329c13
gustavokozlowski <guskozlowskidev@gmail.com>
2024-05-10 21:24:03 -0300

    feat:  atualiza a base do component topic

--- ts/src/app/components/topic/topic.component.ts
+++ ts/src/app/components/topic/topic.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topic.component.scss']
 })
 export class TopicComponent implements OnInit {
-
+  icons = {
+    add: "../../../assets/images/icone-criar.png",
+    hand: '../../../assets/images/icone1.jpg',
+    questions: '../../../assets/images/icone2.jpg',
+    idea: '../../../assets/images/icone3.jpg',
+  }
   constructor() { }
 
   ngOnInit(): void {
